d3050f81db9799a82d156a4a021503f2c686f5c6
Pepe García <jl.garhdez@gmail.com>
2017-01-16 12:04:28 +0100

    add methods

---
+++ src/main/scala/hammock/Method.scala
@@ -0,0 +1,32 @@
+package hammock
+
+trait Method {
+  def name: String
+}
+
+object Method {
+  case object OPTIONS extends Method {
+    def name: String = "OPTIONS"
+  }
+  case object GET extends Method {
+    def name: String = "GET"
+  }
+  case object HEAD extends Method {
+    def name: String = "HEAD"
+  }
+  case object POST extends Method {
+    def name: String = "POST"
+  }
+  case object PUT extends Method {
+    def name: String = "PUT"
+  }
+  case object DELETE extends Method {
+    def name: String = "DELETE"
+  }
+  case object TRACE extends Method {
+    def name: String = "TRACE"
+  }
+  case object CONNECT extends Method {
+    def name: String = "CONNECT"
+  }
+}
