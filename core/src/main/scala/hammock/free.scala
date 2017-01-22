597a9ff141ee474ae016938fe2b74df8fbcb6231
Pepe García <jl.garhdez@gmail.com>
2017-01-22 13:17:06 +0100

    Implement all HTTP methods

--- core/src/main/scala/hammock/free.scala
+++ core/src/main/scala/hammock/free.scala
@@ -41,64 +41,45 @@ object free {
     def trans[F[_]](implicit httpClient: HttpClient, ME: MonadError[F, Throwable]) = transK andThen λ[Kleisli[F, HttpClient, ?] ~> F](_.run(httpClient))
 
     def transK[F[_]](implicit ME: MonadError[F, Throwable]): HttpRequestF ~> Kleisli[F, HttpClient, ?] = λ[HttpRequestF ~> Kleisli[F, HttpClient, ?]](_ match {
-      case Options(url, headers, body) => Kleisli { client =>
-        ME.catchNonFatal {
-          ???
-        }
-      }
-      case Get(url, headers, body) => Kleisli { client =>
-        ME.catchNonFatal {
-          val req = new HttpGet(url)
-
-          headers.foreach {
-            case (k, v) => 
-              req.addHeader(k, v);
-          }
-
-          val resp = client.execute(req)
-          
-          val body = responseContentToString(resp.getEntity().getContent())
-          val status = Status.Statuses.getOrElse(resp.getStatusLine.getStatusCode, throw new Exception) // todo: This is shitty
-          val responseHeaders = resp.getAllHeaders().map(h => h.getName -> h.getValue).toMap
-
-          HttpResponse(status, responseHeaders, body)
-        }
-      }
-      case Head(url, headers, body) => Kleisli { client =>
-        ME.catchNonFatal {
-          ???
-        }
-      }
-      case Post(url, headers, body) => Kleisli { client =>
-        ME.catchNonFatal {
-          ???
-        }
-      }
-      case Put(url, headers, body) => Kleisli { client =>
-        ME.catchNonFatal {
-          ???
-        }
-      }
-      case Delete(url, headers, body) => Kleisli { client =>
-        ME.catchNonFatal {
-          ???
-        }
-      }
-      case Trace(url, headers, body) => Kleisli { client =>
-        ME.catchNonFatal {
-          ???
-        }
-      }
-      case Connect(url, headers, body) => Kleisli { client =>
-        ME.catchNonFatal {
-          ???
-        }
-      }
+      case req@Options(url, headers, body) => doReq(req)
+      case req@Get(url, headers, body) => doReq(req)
+      case req@Head(url, headers, body) => doReq(req)
+      case req@Post(url, headers, body) => doReq(req)
+      case req@Put(url, headers, body) => doReq(req)
+      case req@Delete(url, headers, body) => doReq(req)
+      case req@Trace(url, headers, body) => doReq(req)
     })
+
+    private def doReq[F[_]](reqF: HttpRequestF[HttpResponse])(implicit ME: MonadError[F, Throwable]): Kleisli[F, HttpClient, HttpResponse] = Kleisli { client =>
+      ME.catchNonFatal {
+        val req = getApacheRequest(reqF)
+        reqF.headers.foreach {
+          case (k, v) =>
+            req.addHeader(k, v);
+        }
+
+        val resp = client.execute(req)
+        
+        val body = responseContentToString(resp.getEntity().getContent())
+        val status = Status.Statuses.getOrElse(resp.getStatusLine.getStatusCode, throw new Exception) // todo: This is shitty
+        val responseHeaders = resp.getAllHeaders().map(h => h.getName -> h.getValue).toMap
+
+        HttpResponse(status, responseHeaders, body)
+      }
+    }
   }
 
+  private def getApacheRequest(f: HttpRequestF[HttpResponse]): HttpUriRequest = f match {
+    case Get(url, headers, body) => new HttpGet(url)
+    case Options(url, headers, body) => new HttpOptions(url)
+    case Head(url, headers, body) => new HttpHead(url)
+    case Post(url, headers, body) => new HttpPost(url)
+    case Put(url, headers, body) => new HttpPut(url)
+    case Delete(url, headers, body) => new HttpDelete(url)
+    case Trace(url, headers, body) => new HttpTrace(url)
+  }
 
-  def responseContentToString(content: InputStream): String = {
+  private def responseContentToString(content: InputStream): String = {
     val rd = new BufferedReader(new InputStreamReader(content))
 
     Stream.continually(rd.readLine()).takeWhile(_ != null).mkString("")
