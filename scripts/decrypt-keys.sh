85ca7e11a256630de61f798cd096a07d5811353a
pepegar <jl.garhdez@gmail.com>
2017-01-31 17:13:38 +0100

    delete typo in decrypt-keys

--- scripts/decrypt-keys.sh
+++ scripts/decrypt-keys.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-openssl aes-256-cbc -K $encrypted_4177d4fd1dcb_key -iv $encrypted_4177d4fd1dcb_iv -in travis-deploy-key.enc -out travis-deploy-key -dñ
+openssl aes-256-cbc -K $encrypted_4177d4fd1dcb_key -iv $encrypted_4177d4fd1dcb_iv -in travis-deploy-key.enc -out travis-deploy-key -d;
 chmod 600 travis-deploy-key;
 cp travis-deploy-key ~/.ssh/id_rsa;
 
