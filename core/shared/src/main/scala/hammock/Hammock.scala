d479a9ee7ddd0a32cdb8d486426835b854134dd0
Pepe García <jl.garhdez@gmail.com>
2017-02-15 17:47:23 +0100

    Fix algebra (#15)
    * delete body from cases of the ADT that cannot have one
    
    * fix constructors and smart constructors of the algebra
    
    * fix build in docs.md

--- core/shared/src/main/scala/hammock/Hammock.scala
+++ core/shared/src/main/scala/hammock/Hammock.scala
@@ -11,23 +11,23 @@ import hi.Opts
 object Hammock {
 
   def request(method: Method, url: String, headers: Map[String, String]): HttpRequestIO[HttpResponse] = method match {
-    case Method.OPTIONS => Ops.options(url, headers, None)
-    case Method.GET => Ops.get(url, headers, None)
-    case Method.HEAD => Ops.head(url, headers, None)
+    case Method.OPTIONS => Ops.options(url, headers)
+    case Method.GET => Ops.get(url, headers)
+    case Method.HEAD => Ops.head(url, headers)
     case Method.POST => Ops.post(url, headers, None)
     case Method.PUT => Ops.put(url, headers, None)
-    case Method.DELETE => Ops.delete(url, headers, None)
-    case Method.TRACE => Ops.trace(url, headers, None)
+    case Method.DELETE => Ops.delete(url, headers)
+    case Method.TRACE => Ops.trace(url, headers)
   }
 
   def request[A : Codec](method: Method, url: String, headers: Map[String, String], body: Option[A]): HttpRequestIO[HttpResponse] = method match {
-    case Method.OPTIONS => Ops.options(url, headers, body.map(Codec[A].encode))
-    case Method.GET => Ops.get(url, headers, body.map(Codec[A].encode))
-    case Method.HEAD => Ops.head(url, headers, body.map(Codec[A].encode))
+    case Method.OPTIONS => Ops.options(url, headers)
+    case Method.GET => Ops.get(url, headers)
+    case Method.HEAD => Ops.head(url, headers)
     case Method.POST => Ops.post(url, headers, body.map(Codec[A].encode))
     case Method.PUT => Ops.put(url, headers, body.map(Codec[A].encode))
-    case Method.DELETE => Ops.delete(url, headers, body.map(Codec[A].encode))
-    case Method.TRACE => Ops.trace(url, headers, body.map(Codec[A].encode))
+    case Method.DELETE => Ops.delete(url, headers)
+    case Method.TRACE => Ops.trace(url, headers)
   }
 
   def withOpts(method: Method, url: String, opts: Opts): HttpRequestIO[HttpResponse] = {
